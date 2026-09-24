3421eb0f244645a81b6d0288f1c403cbc7ce8572
Josh Holtz <joshholtz@google.com>
2026-09-24 15:20:30 +0000

    refactor(terraform): reset workspace infrastructure to clean baseline
    Remove starter Argolis Compute Engine instance, service account, and VM-specific variables/outputs. Retain GCP provider configuration and baseline variables (project_id, region, zone).

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "project_id" {
-  description = "The GCP project ID (Argolis demo project)"
+  description = "The GCP project ID"
   type        = string
 }
 
@@ -13,40 +13,4 @@ variable "zone" {
   description = "Default GCP zone for compute resources"
   type        = string
   default     = "us-central1-a"
-}
-
-variable "instance_name" {
-  description = "Name for the Argolis Compute Engine instance"
-  type        = string
-  default     = "argolis-dev-vm"
-}
-
-variable "machine_type" {
-  description = "Machine type for the Compute Engine instance"
-  type        = string
-  default     = "e2-standard-4"
-}
-
-variable "boot_disk_image" {
-  description = "OS boot disk image for the instance"
-  type        = string
-  default     = "debian-cloud/debian-12"
-}
-
-variable "boot_disk_size_gb" {
-  description = "Boot disk size in GB"
-  type        = number
-  default     = 50
-}
-
-variable "network" {
-  description = "VPC network to attach the instance to"
-  type        = string
-  default     = "default"
-}
-
-variable "tags" {
-  description = "Network tags to apply to the instance"
-  type        = list(string)
-  default     = ["argolis-dev", "http-server", "https-server"]
 }
